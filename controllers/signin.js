3e9c931ac12013acd7c6bc47c743fef9fd4658cd
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-19 14:44:04 -0700

    added heroku redis and update redis.createClient call in signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const redis = require('redis');
 
 // setup redis:
-const redisClient = redis.createClient(process.env.REDIS_URI);
+const redisClient = redis.createClient(process.env.REDIS_URL);
 
 
 
